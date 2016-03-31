6fa4493b134c29983c2e4a63eb4aa0456cc7d3cd
biroorsolya <birorso@gmail.com>
2016-03-31 23:27:52 +0200

    It's just a skeleton yet

---
+++ Contacts/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string Type;
+        public Person(string name, string address, int age, string telephone, string type)
+        {
+            Name = name;
+            Address = address;
+            Age = age;
+            TelephoneNumber = telephone;
+            Type = type;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "Address: " + Address + "Age: " + Age + " " + Type + ": " + TelephoneNumber;
+        }
+    }
+}
---
+++ Contacts/TelephoneDirectory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts
+{
+    class TelephoneDirectory
+    {
+        public List<Person> PersonList;
+        public TelephoneDirectory()
+        {
+            PersonList = new List<Person>(50);
+        }
+
+        public void AddPerson(Person person)
+        {
+            PersonList.Add(person);
+        }
+
+        public override string ToString()
+        {
+            return "People: " + PersonList;
+        }
+    }
+}
